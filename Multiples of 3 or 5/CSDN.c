96964621bf9bc9cd129a8e5c8194f5ebad70ba62
Egor Lee <89340462+appleiphonedddd@users.noreply.github.com>
2022-12-02 18:50:22 +0800

    Add files via upload

---
+++ Multiples of 3 or 5/CSDN.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main()
+{
+    int number, i,sum;
+    printf("Enter a random number");
+    scanf("%d", &number);
+
+    for (i = 0; i < number; i++)
+    {
+        if (i%5 == 0 || i%3 == 0 )
+        {
+            sum += i;
+        }
+
+    }
+    printf("%d",sum);
+}
---
+++ Multiples of 3 or 5/CSDN.py
@@ -0,0 +1,8 @@
+number = int(input())
+sum=0
+
+for i in range(1,number):
+    if i%5==0 or i%3==0:
+        sum+=i
+
+print(sum)
